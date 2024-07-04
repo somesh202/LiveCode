aa52d861a5e3a567eed977f1d764849d70059d5a
somesh202 <somesh.ranjan.met20@itbhu.ac.in>
2024-07-05 01:35:08 +0530

    minor fix

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,10 @@ module.exports = {
     collectCoverage: true,
     coverageDirectory: 'coverage',
     coverageReporters: ['lcov', 'text', 'html'],
+    moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+    },
     transform: {
         '^.+\\.jsx?$': 'babel-jest'
     }
   };
-  
