51a5ee5406fdea69a9742b56234003be8a43d235
somesh202 <somesh.ranjan.met20@itbhu.ac.in>
2024-07-05 01:08:31 +0530

    minor fix

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useRef, useEffect } from 'react';
 import toast from 'react-hot-toast';
 import ACTIONS from '../Actions';
